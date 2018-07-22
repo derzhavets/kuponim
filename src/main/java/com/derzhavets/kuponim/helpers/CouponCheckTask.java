936039bc19d7c21a737a4cffc12b0105942289da
Michael Derzhavets <derzhavets@gmail.com>
2018-07-22 14:07:34 +0300

    Add task execution

--- src/main/java/com/derzhavets/kuponim/helpers/CouponCheckTask.java
+++ src/main/java/com/derzhavets/kuponim/helpers/CouponCheckTask.java
@@ -1,10 +1,14 @@
 package com.derzhavets.kuponim.helpers;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.derzhavets.kuponim.dao.CouponDao;
+import com.derzhavets.kuponim.entities.Coupon;
 
 @Component
 public class CouponCheckTask {
@@ -14,6 +18,7 @@ public class CouponCheckTask {
 	
 	@Scheduled(fixedRate = 1000 * 3600 * 24)
 	public void deleteExpiredCoupons() {
-		
+		List<Coupon> coupons = couponDao.getExpiredFrom(LocalDate.now());
+		couponDao.deleteAll(coupons);
 	}
 }
